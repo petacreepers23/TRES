2fa8eae96c3693e3559830886559f2d44001f5cc
Carlos <krlsmj@protonmail.com>
2021-05-08 14:57:24 +0200

    Añadido un listado de los dispositivos pci. Simplificado un print. ficheros random que no se que hacen .

--- TRES/drivers/pci.hpp
+++ TRES/drivers/pci.hpp
@@ -1,3 +1,21 @@
 #pragma once
 
+// Esto no tendría que estar aquí.
+typedef signed char __int8_t;
+typedef unsigned char __uint8_t;
+typedef signed short int __int16_t;
+typedef unsigned short int __uint16_t;
+typedef signed int __int32_t;
+typedef unsigned int __uint32_t;
+
+typedef __uint8_t uint8_t;
+typedef __uint16_t uint16_t;
+typedef __uint32_t uint32_t;
+
 int ls_pci();
+uint32_t get_PCI_register(int bus, int slot, int func, int reg);
+int print_slot(int bus, int slot);
+void print_bus(int bus);
+int print_device(int bus, int slot, int func);
+int print_regions(int bus, int slot, int func, int bridge);
+void print_pci();
